88db68dac2fafcfd05058f85345e55f1e1309efd
oxypteros <oxypteros@gmail.com>
2025-12-02 11:02:37 +0100

    Implement smooth scrolling with Lenis on layout mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { onMount } from 'svelte';
+	import Lenis from 'lenis';
+	import 'lenis/dist/lenis.css';
+	
 	let { children } = $props();
+
+	onMount(() => {
+		const lenis = new Lenis({
+			duration: 1.5,
+			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+		});
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+
+		return () => {
+			lenis.destroy();
+		};
+	});
 </script>
 
 <svelte:head>
