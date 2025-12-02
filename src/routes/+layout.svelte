424d6bd1c44fd0396cce998f3e097090ebed3feb
oxypteros <oxypteros@gmail.com>
2025-12-02 12:20:14 +0100

    style: Format code for consistency and readability in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { onMount } from 'svelte';
-	import Lenis from 'lenis';
-	import 'lenis/dist/lenis.css';
-	
-	let { children } = $props();
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { onMount } from "svelte";
+  import Lenis from "lenis";
+  import "lenis/dist/lenis.css";
 
-	onMount(() => {
-		const lenis = new Lenis({
-			duration: 1.5,
-			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-		});
-		function raf(time: number) {
-			lenis.raf(time);
-			requestAnimationFrame(raf);
-		}
+  let { children } = $props();
 
-		requestAnimationFrame(raf);
+  onMount(() => {
+    const lenis = new Lenis({
+      duration: 1.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
 
-		return () => {
-			lenis.destroy();
-		};
-	});
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  });
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
