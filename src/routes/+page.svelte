4d6b39c868fc089c7b4c68767047e5f2d1e55ba5
oxypteros <oxypteros@gmail.com>
2025-12-02 11:58:14 +0100

    feat: Import Header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,16 @@
-<svelte:options runes={true} />
-
 <script lang="ts">
   import SEO from "$lib/components/SEO.svelte";
+  import Header from "$lib/components/Header.svelte";
 </script>
 
 <SEO
   title="Rachel Stone | Svelte 5 Demo by Oxypteros"
-  repoUrl="https://github.com/oxypteros/figma-to-svelte-rachel"
+  repoUrl="https://github.com/oxypteros/figma-to-svelte5-rachel"
 />
 <div class="relative min-h-[200vh]">
+  <Header />
   <main class="bg-wh-100 dark:bg-bl-90 relative z-20">
     <div class="bg-wh-100 dark:bg-bl-90 z-30 relative"></div>
   </main>
-  <footer>
-  </footer>
+  <footer></footer>
 </div>
