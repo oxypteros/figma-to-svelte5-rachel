63cc47f4f303913a1a0a5c8c7f4d045d5dfc20a5
oxypteros <oxypteros@gmail.com>
2025-12-02 11:02:55 +0100

    Replace default welcome message with SEO component and layout structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:options runes={true} />
+
+<script lang="ts">
+  import SEO from "$lib/components/SEO.svelte";
+</script>
+
+<SEO
+  title="Rachel Stone | Svelte 5 Demo by Oxypteros"
+  repoUrl="https://github.com/oxypteros/figma-to-svelte-rachel"
+/>
+<div class="relative min-h-[200vh]">
+  <main class="bg-wh-100 dark:bg-bl-90 relative z-20">
+    <div class="bg-wh-100 dark:bg-bl-90 z-30 relative"></div>
+  </main>
+  <footer>
+  </footer>
+</div>
