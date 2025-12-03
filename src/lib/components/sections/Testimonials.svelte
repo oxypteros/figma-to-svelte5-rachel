9d92450569273cfe69b6e61e2f68e19b941a93c8
oxypteros <oxypteros@gmail.com>
2025-12-03 17:23:16 +0100

    feat: Create testimonials component that import data from `testinomials.ts`

---
+++ src/lib/components/sections/Testimonials.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { testimonials } from "$lib/data/testimonials";
+  import Star from "~icons/rachel/star";
+
+  // Render only the first testimonial from the testimonials json
+  const current = testimonials[0];
+</script>
+
+<section class="bg-wh-100 w-full py-14 md:py-24">
+  <h2 class="sr-only">Testimonials</h2>
+  <div class="max-w-desktop-container mx-auto flex w-full px-4 md:items-center md:justify-center">
+    <div class="flex flex-col gap-4">
+      <div class="flex">
+        <span class="sr-only">{current.stars} stars out of five</span>
+        {#each Array(current.stars) as _}
+          <Star class="text-yellow size-5" />
+        {/each}
+      </div>
+      <q class="text-h6 text-bl-100 md:text-h3 text-pretty sm:w-130 md:w-201.5">{current.quote}</q>
+      <div class="flex items-center gap-4">
+        <img
+          width="64"
+          height="64"
+          src={current.image}
+          alt={current.alt}
+          loading="lazy"
+          decoding="async"
+          class="roounded-full bg-wh-100 object-cover"
+        />
+        <div class="flex flex-col">
+          <span class="text-1 text-bl-100">{current.name}</span>
+          <span class="text-3 text-bl-50">{current.program} - {current.date}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
