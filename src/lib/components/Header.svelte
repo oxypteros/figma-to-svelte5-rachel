d4ade38d936ff9060f20519b53884ac6e7166e0f
oxypteros <oxypteros@gmail.com>
2025-12-04 13:07:21 +0100

    feat: Add dark theme to Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -14,16 +14,16 @@
       class="bg-bl-100/65 flex items-center justify-center rounded-full py-2 backdrop-blur-sm px-4"
     >
       <Logo class="text-wh-100" />
-      <Button variant="icon" size="icon-lg" class="bg-transparent" aria-label="Open Menu">
+      <Button variant="icon" size="icon-lg" class="bg-transparent dark:bg-transparent" aria-label="Open Menu">
         <Hamburger class="text-wh-100 h-4 w-5" />
       </Button>
     </nav>
     <Button onclick={toggleMode} variant="icon" size="icon-lg" class="group">
       <SunIcon
-        class="text-wh-100 size-5 scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90 group-hover:text-bl-100 duration-300"
+        class="text-wh-100 dark:text-bl-100 size-5 scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90 group-hover:text-bl-100 duration-300"
       />
       <MoonIcon
-        class="absolute size-5 text-wh-100 scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0 group-hover:text-bl-100 duration-300"
+        class="absolute size-5 text-wh-100 dark:text-bl-100 scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0 group-hover:text-bl-100 duration-300 "
       />
       <span class="sr-only">Toggle theme</span>
     </Button>
