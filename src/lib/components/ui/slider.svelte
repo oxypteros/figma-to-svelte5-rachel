253660373b594a5944949466104eca8f882af1cd
oxypteros <oxypteros@gmail.com>
2025-12-04 17:19:32 +0100

    feat: Create slider component for before/after comparison

---
+++ src/lib/components/ui/slider.svelte
@@ -0,0 +1,59 @@
+<script>
+  import Handle from "~icons/rachel/handle";
+  let sliderValue = $state(50);
+  let { before, after } = $props();
+</script>
+
+<div
+  class="group relative h-186 w-full max-w-186 touch-none overflow-hidden rounded-md select-none mx-auto"
+>
+  <!-- Background (After) -->
+  <img
+    src="/img/results/{after}"
+    decoding="auto"
+    loading="lazy"
+    alt="After result"
+    class="absolute inset-0 h-full w-full object-cover object-center"
+  />
+
+  <!--  Foreground (Before) -->
+  <img
+    src="/img/results/{before}"
+    decoding="auto"
+    loading="lazy"
+    alt="Before result"
+    class="absolute inset-0 h-full w-full object-cover object-center"
+    style="clip-path: inset(0 {100 - sliderValue}% 0 0);"
+  />
+
+  <!-- Divider -->
+  <div
+    class="bg-wh-100/40 pointer-events-none absolute inset-y-0 w-px"
+    style="left: {sliderValue}%; transform: translateX(-50%);"
+  >
+    <!-- Handle -->
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Handle />
+    </div>
+  </div>
+
+  <!-- Badges-->
+  <span
+    class="border-wh-100/24 bg-wh-100/48 text-3 text-bl-100 absolute bottom-6 left-6 rounded-[6px] border px-4 py-[3px]"
+    >Before</span
+  >
+  <span
+    class="border-wh-100/24 bg-wh-100/48 text-3 text-bl-100 absolute right-6 bottom-6 rounded-[6px] border px-4 py-[3px]"
+    >After</span
+  >
+
+  <!-- Input -->
+  <input
+    type="range"
+    min="0"
+    max="100"
+    bind:value={sliderValue}
+    class="absolute inset-0 z-20 h-full w-full cursor-ew-resize opacity-0"
+    aria-label="Compare before and after images"
+  />
+</div>
