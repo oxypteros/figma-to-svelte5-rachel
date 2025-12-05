29c0d61db3d5277eb34f7ebf6d9cd2abca67d1b6
oxypteros <oxypteros@gmail.com>
2025-12-05 14:26:37 +0100

    feat: Create demo contact json

---
+++ src/lib/data/contact.ts
@@ -0,0 +1,9 @@
+export const contactConfig = {
+    contact: {
+        phoneDisplay: "+1 (332) 000-0000",
+        phoneNumber: "+13320000000",
+        whatsapp: "13320000000", 
+        email: "hello@oxypteros.com",
+        hours: "Mon-Fri, 8:00-17:00 (CET)"
+    }
+};
