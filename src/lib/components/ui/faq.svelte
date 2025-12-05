fe9c100575e43a0df124021bd51bb03fdeec1a1b
oxypteros <oxypteros@gmail.com>
2025-12-05 12:46:18 +0100

    feat: Create faq data component

---
+++ src/lib/components/ui/faq.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Plus from "~icons/rachel/plus";
+  import Minus from "~icons/rachel/minus";
+  import type { FaqItem } from "$lib/data/faq";
+
+  let { items } = $props<{ items: FaqItem[] }>();
+</script>
+
+<div class="text-3">
+  {#each items as item}
+    <details class="group">
+      <summary
+        class="flex cursor-pointer list-none items-center justify-between gap-2 py-6 [&::-webkit-details-marker]:hidden"
+      >
+        <span>{item.question}</span>
+
+        <Plus class="transition-transform duration-300 group-open:hidden" />
+        <Minus class="hidden transition-transform duration-300 group-open:block" />
+      </summary>
+
+      <p class="text-bl-60 dark:text-wh-90/80 pr-2 pb-6 pl-4 text-pretty">
+        {item.answer}
+      </p>
+    </details>
+  {/each}
+</div>
