d8a25bde791a2aee0490d34740bfb6415d29af64
oxypteros <oxypteros@gmail.com>
2025-12-05 18:31:01 +0100

    feat: Creat nav-menu component

---
+++ src/lib/components/ui/nav-menu.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { menuItems } from "$lib/data/menu";
+  let { onclick } = $props<{ onclick?: () => void }>();
+</script>
+
+<ul class="flex flex-col gap-4 text-left min-w-32 p-4">
+  {#each menuItems as item}
+    <li>
+      <a
+        href={item.href}
+        {onclick}
+        class="text-1 block text-wh-100 hover:text-primary transition-colors duration-300"
+      >
+        {item.label}
+      </a>
+    </li>
+  {/each}
+</ul>
