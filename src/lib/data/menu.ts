6b2f6655fe0cafbe9da37f80b3acef52af435b44
oxypteros <oxypteros@gmail.com>
2025-12-05 18:19:19 +0100

    feat: Create menu entries data json

---
+++ src/lib/data/menu.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "#introduction-anchor" },
+  { label: "Services", href: "#services-anchor" },
+  { label: "Programs", href: "#programs-anchor" },
+  { label: "Results", href: "#results-anchor" },
+  { label: "Contact", href: "#contact-anchor" }
+];
