98148ca085b520c0c627f3813226d60378ebc5d7
oxypteros <oxypteros@gmail.com>
2025-12-06 15:15:52 +0100

    feat: Add FooterMenu component and footerMenuItems array

---
+++ src/lib/components/ui/footer-menu.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { footerMenuItems } from "$lib/data/footer-menu";
+</script>
+
+<ul class="text-1 space-y-4 text-center md:flex md:flex-row md:space-y-0 md:space-x-4">
+  {#each footerMenuItems as item}
+    <li>
+      <a
+        href={item.href}
+        class="text-1 block text-wh-100 hover:text-primary transition-colors duration-300"
+        aria-label={item.label}
+      >
+        {item.id}
+      </a>
+    </li>
+  {/each}
+</ul>
