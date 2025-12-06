18e35402a4fed6f870e9085df7d8781afa5f4383
oxypteros <oxypteros@gmail.com>
2025-12-06 15:22:17 +0100

    refactor: Rename nav-menu to header-menu for name consistency

--- src/lib/components/ui/header-menu.svelte
+++ src/lib/components/ui/header-menu.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-  import { menuItems } from "$lib/data/menu";
+  import { headerMenuItems } from "$lib/data/header-menu";
   let { onclick } = $props<{ onclick?: () => void }>();
 </script>
 
-<ul class="flex flex-col gap-4 text-left min-w-32 p-4">
-  {#each menuItems as item}
+<ul class="flex flex-col gap-4 text-left min-w-32 py-4">
+  {#each headerMenuItems as item}
     <li>
       <a
         href={item.href}
         {onclick}
         class="text-1 block text-wh-100 hover:text-primary transition-colors duration-300"
+        aria-label={item.label}
       >
-        {item.label}
+        {item.id}
       </a>
     </li>
   {/each}
